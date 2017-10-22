ce456708bdfcf2a235f0927b120b06f177b68b97
Wendy <lx.wendychen@gmail.com>
2017-10-22 14:36:22 +1100

    Wendy-Solution

--- pixart.js
+++ pixart.js
@@ -1,12 +1,21 @@
+
+var color;
+var url;
+var toggle = "";
+
 //commit 1
 $("#set-color").click(function () {
+  toggle = "color";
   event.preventDefault();
-  var color = $("#color-field").val()
+  color = $("#color-field").val()
   $(".brush").css("background-color", color);
+  $(".brush").css('background-image', 'none');
+
 });
 
 //commit 2
 $('#set-color').keydown(function (event) {
+  toggle = "color";
     if (event.keyCode === 13) {
         $(".brush").css("background-color", color);
     }
@@ -14,10 +23,53 @@ $('#set-color').keydown(function (event) {
 
 //commit 3
 for (i = 0; i < 1000; i++) {
-  $("body").append($('<div class="square"></div>')) ;
+  $("body").append($('<div class="square" id='+i+' ></div>')) ;
 }
 
 //commit 4
-$('body').on('click', function(){
-    alert($(this).attr("class"));
+// $('.square').on('click', function(){
+//     var sqID = $(this).attr("id");
+//     $('#'+sqID+'.square').css("background-color", "green");
+// });
+
+//commit 5
+// $('.square').on('click', function(){
+//     var sqID = $(this).attr("id");
+//     $('#'+sqID+'.square').css("background-color", color);
+// });
+
+//commit 6
+function colorGrids (sqID){
+  $('#'+sqID+'.square').css("background-color", color);
+  $('#'+sqID+'.square').css('background-image', 'none');
+}
+
+function imageGrids (sqID,url){
+  $('#'+sqID+'.square').css('background-image', 'url(' + url + ')');
+}
+
+//commit 7
+$("#set-poster").click(function () {
+  toggle = "movie";
+  event.preventDefault();
+  var movie = $("#movie-name").val();
+  $.ajax({
+    method: "GET",
+    url: "http://www.omdbapi.com/?",
+    data: { t: movie, apikey: "2f6435d9" }
+  }).done(function(msg) {
+    var poster_URL = msg['Poster'];
+    url = poster_URL;
+    $(".brush").css('background-image', 'url(' + url + ')');
+  });
 });
+
+$('.square').mouseover (function(){
+  var sqID = $(this).attr("id");
+  if(toggle === "movie"){
+    imageGrids (sqID,url);
+  }
+  if(toggle === "color"){
+    colorGrids (sqID);
+  }
+})  ;
